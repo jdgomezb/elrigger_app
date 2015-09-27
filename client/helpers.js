16b93b7d5d72d7a2bc0d133c5bcee1ecbe210866
JD Gomez <jdgomezb@gmail.com>
2015-09-27 12:30:25 -0500

    Basic markup for menu, dashboard, search and main wrapper

---
+++ client/helpers.js
@@ -0,0 +1,25 @@
+pluralize = function(n, thing, options) {
+  var plural = thing;
+  if (_.isUndefined(n)) {
+    return thing;
+  } else if (n !== 1) {
+    if (thing.slice(-1) === 's')
+      plural = thing + 'es';
+    else
+      plural = thing + 's';
+  }
+
+  if (options && options.hash && options.hash.wordOnly)
+    return plural;
+  else
+    return n + ' ' + plural;
+}
+
+Handlebars.registerHelper('pluralize', pluralize);
+
+Handlebars.registerHelper('activePage', function() {
+  // includes Spacebars.kw but that's OK because the route name ain't that.
+  var routeNames = arguments;
+
+  return _.include( routeNames, Router.current().route.getName() ) && 'active';
+});
